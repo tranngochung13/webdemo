43880296033164ac095142ea37548d32c5172c14
hung.tran <tranngochung1302@gmail.com>
2018-12-20 16:28:57 +0700

    commit

---
+++ demo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<!-- <?php 
+		$a = 13;
+		$b = 2;
+		$c = $a + $b;
+		echo $c;
+	 ?> -->
+	<!-- is_float biết được function mang kiểu gì -->
+	 <?php 
+	 $first_number = 1;
+	 $second_number = 1.1;
+	 $result = is_float($first_number + $second_number);
+	 echo $result;
+	  ?>
+</body>
+</html>
